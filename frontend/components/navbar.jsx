71ca6504d2fb09c13c8e9de8f35f91417faf9632
Dimas Bintang <dimasbintang1818@gmail.com>
2025-11-06 09:02:31 +0700

    feat : add navbar from shadcn.io

---
+++ frontend/components/navbar.jsx
@@ -0,0 +1,8 @@
+import { Navbar04 } from '@/components/ui/shadcn-io/navbar-04';
+
+const Navbar = () => (
+  <nav className="relative w-full bg-transparent">
+    <Navbar04 />
+  </nav>
+);
+export default Navbar;
