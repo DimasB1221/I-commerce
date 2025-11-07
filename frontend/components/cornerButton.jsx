b24ee306c6b9fc5c3cbd644dd28f9066976a79c4
Dimas Bintang <dimasbintang1818@gmail.com>
2025-11-07 20:43:04 +0700

    feat : add cornerButton component

---
+++ frontend/components/cornerButton.jsx
@@ -0,0 +1,9 @@
+import { CornerAccentButton } from "@/components/ui/shadcn-io/corner-accent-button"
+
+export default function CornerButton() {
+  return (
+    <div className="w-full p-6 flex justify-center">
+      <CornerAccentButton />
+    </div>
+  )
+}
