25125ea6fbb7930b813541c1e669678dca77d643
Dimas Bintang <dimasbintang1818@gmail.com>
2025-11-11 22:54:22 +0700

    feat : add dummy data

---
+++ frontend/lib/dummy.js
@@ -0,0 +1,40 @@
+const Products = [
+  {
+    id: 1,
+    name: "Iphone 11",
+    price: 5000000,
+    description: "Ini iphone 11 ya, kan sudah tau",
+    categories: "Smartphone",
+    image:
+      "https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aXBob25lJTIwMTF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
+  },
+  {
+    id: 2,
+    name: "Iphone 12",
+    price: 6000000,
+    description: "Ini iphone 12 ya, kan sudah tau",
+    categories: "Smartphone",
+    image:
+      "https://images.unsplash.com/photo-1604054923518-e491a9a6afbb?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGlwaG9uZSUyMDEyfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=500",
+  },
+  {
+    id: 3,
+    name: "Iphone 13",
+    price: 7000000,
+    description: "Ini iphone 13 ya, kan sudah tau",
+    categories: "Smartphone",
+    image:
+      "https://images.unsplash.com/photo-1726574686436-5ef90358e032?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aXBob25lJTIwMTN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
+  },
+  {
+    id: 4,
+    name: "Iphone 14",
+    price: 7000000,
+    description: "Ini iphone 11 ya, kan sudah tau",
+    categories: "Smartphone",
+    image:
+      "https://images.unsplash.com/photo-1726574686436-5ef90358e032?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aXBob25lJTIwMTN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
+  },
+];
+
+export default Products;
