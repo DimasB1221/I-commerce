82046d9f52516aa7d62dd6b19c3f50e8c7293f54
Dimas Bintang <dimasbintang1818@gmail.com>
2025-11-11 22:53:53 +0700

    feat : add apiusing axios

---
+++ frontend/lib/api.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { redirect } from "next/navigation";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  headers: "application/json",
+  timeout: 10000,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        config.headers.authorization = `Bearer${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.status?.message === 401 && typeof window !== undefined) {
+      localStorage.removeItem("token");
+      redirect("/login");
+    }
+
+    const customError = new Error(
+      error.status?.message || error.message || "Request Failed"
+    );
+
+    customError.data = error.response?.data;
+    customError.status = error.response?.status;
+
+    customError.all = error;
+    return Promise.reject(customError.data);
+  }
+);
+
+export default api;
