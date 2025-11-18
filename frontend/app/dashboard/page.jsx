a5595d6a27370eedb8e7df623234153d59b6060d
Dimas Bintang <dimasbintang1818@gmail.com>
2025-11-18 18:38:29 +0700

    feat : add sidebar and card info admin

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -1,13 +1,35 @@
 import Sidebar1 from "@/components/sidebar"
+import { Store } from 'lucide-react';
 
 
 
 export default function Dashboard (){
     return (
         <>
-            <header>
-                <Sidebar1 />
+            <header className="">
+                <Sidebar1/>
             </header>
+            <main className="h-screen bg-neutral-100 py-4">
+                <ul className="admin-info grid gap-5 w-[95%] m-auto">
+                    <li className=" info-card shadow-top-bottom-soft"> 
+                        <p className="col-span-2 ">Product</p>
+                        <h1 className="">100</h1>
+                        <Store className=" self-start justify-self-end"/>
+                    </li>
+                    <li className="info-card shadow-top-bottom-soft">
+                        <p className="col-span-3">Stock</p>
+                        <h1>100</h1>
+                        <Store className="self-start justify-self-end "/>
+                    </li>
+                    <li className="info-card shadow-top-bottom-soft">
+                        <p className="col-span-3">Categories</p>
+                        <h1>100</h1>
+                         <Store className="self-start justify-self-end"/>
+                    </li>
+                </ul>
+            </main>
+
+            
         </>
     )
 }
